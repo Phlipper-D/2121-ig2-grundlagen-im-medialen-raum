2a8ba22b3c06712f6a7378cc7a634e627357d276
Phlipper-D <75977142+Phlipper-D@users.noreply.github.com>
2021-03-16 09:32:28 +0100

    Das was wir gestern erreicht haben

--- client/public/0_empty_example/js/script.js
+++ client/public/0_empty_example/js/script.js
@@ -1,6 +1,12 @@
 // Connecting to server. Don't touch this :-) 
 let socket = io();
 
+function clickedButton() {
+   // console.log("Click auf Button")
+
+    socket.emit('serverEvent', "ost");
+}
+
 
 
 socket.on('connected', function (msg) {
@@ -8,8 +14,30 @@ socket.on('connected', function (msg) {
 });
 
 
-
 // Incoming events 
 socket.on('serverEvent', function (message) {
+ console.log(message)
 
+ let Button1 = document.getElementById("Button1")
+
+ if (message == "süd") {
+    let y = Button1.offsetTop;
+    y = y + 20;
+    Button1.style.top = y + "px";
+ }
+ if (message == "ost") {
+    let y = Button1.offsetLeft;
+    y = y + 20;
+    Button1.style.left = y + "px";
+ }
+ if (message == "nord") {
+    let y = Button1.offsetTop;
+    y = y - 20;
+    Button1.style.top = y + "px";
+ }
+ if (message == "west") {
+    let y = Button1.offsetLeft;
+    y = y - 20;
+    Button1.style.left = y + "px";
+ }
 });
