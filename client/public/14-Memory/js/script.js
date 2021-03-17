3968af0c292409c05a5632683765325f9a4212e4
Phlipper-D <75977142+Phlipper-D@users.noreply.github.com>
2021-03-17 13:54:21 +0100

    13:54

--- client/public/14-Memory/js/script.js
+++ client/public/14-Memory/js/script.js
@@ -1,45 +1,49 @@
 // Connecting to server. Don't touch this :-) 
 let socket = io();
 
- let myPlayerIndex = 0;
+let myPlayerIndex = 0;
 let playerColors = ['#cc0000', '#70a500', '#cc9710', '#008fcc']
 let playerCount = 0;
- let whosTurn = 0;
+let whosTurn = 0;
 
- let cardsPlayed = []
+let cardsPlayed = []
 
-let sounds = ['a', 'a', 'b','b','c','c','d','d','e','e','f','f','g','g','h','h',]
+let sounds = ['a', 'a', 'b', 'b', 'c', 'c', 'd', 'd', 'e', 'e', 'f', 'f', 'g', 'g', 'h', 'h', ]
 
 
 
- function shuffle(sounds) {
-   var currentIndex = sounds.length, temporaryValue, randomIndex;
-    
+function shuffle(sounds) {
+   var currentIndex = sounds.length,
+      temporaryValue, randomIndex;
+
    while (0 !== currentIndex) {
-     randomIndex = Math.floor(Math.random() * currentIndex);
-     currentIndex -= 1;
-     temporaryValue = sounds[currentIndex];
-     sounds[currentIndex] = sounds[randomIndex];
-     sounds[randomIndex] = temporaryValue;
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = sounds[currentIndex];
+      sounds[currentIndex] = sounds[randomIndex];
+      sounds[randomIndex] = temporaryValue;
    }
- 
+
    return sounds;
- }
+}
 
- shuffle(sounds);
- console.log(sounds);
-
-
+shuffle(sounds);
+console.log(sounds);
 
 
- socket.on('newUsersEvent', function (myID, myIndex, userList) {
+
+
+socket.on('newUsersEvent', function (myID, myIndex, userList) {
    console.log("New users event: ");
    console.log("That's me: " + myID);
    console.log("My index in the list: " + myIndex);
    console.log("That's the new users: ");
    console.log(userList);
 
-   socket.emit('serverEvent', {type:"RandomList",sounds});
+   socket.emit('serverEvent', {
+      type: "RandomList",
+      sounds
+   });
 
    playerCount = userList.length;
    myPlayerIndex = myIndex;
@@ -53,8 +57,8 @@ let sounds = ['a', 'a', 'b','b','c','c','d','d','e','e','f','f','g','g','h','h',
 let gridSize = 4;
 $('.wrapper').children().remove();
 $('.wrapper').css("grid-template-columns", "repeat(" + gridSize + ", 130px)");
-for (let i = 0; i < gridSize*gridSize; i++) {
-    $('.wrapper').append('<div class="cell empty"></div>');
+for (let i = 0; i < gridSize * gridSize; i++) {
+   $('.wrapper').append('<div class="cell empty"></div>');
 }
 
 
@@ -65,28 +69,34 @@ function clickedButton(ev) {
 
    let element = $(ev.target);
 
-  if (element.hasClass('border')) {
-         element.removeClass('border');    
-  } else {
-     element.addClass('border')
-  } 
+   if (element.hasClass('border')) {
+      element.removeClass('border');
+   } else {
+      element.addClass('border')
+   }
 
    //  socket.emit('serverEvent', "süd");
 }
 
 
-$('.cell').click(function() {
+$('.cell').click(function () {
    console.log(myPlayerIndex)
    if (whosTurn == myPlayerIndex && $(this).hasClass("empty")) {
-       // console.log(this);
-       socket.emit('serverEvent', {type:"played", playerIndex:myPlayerIndex, cellIndex:$(this).index()});  
-       socket.emit('serverEvent', {type:"sound", playerIndex:myPlayerIndex, cellIndex:$().index()});
-      }
+      // console.log(this);
+      socket.emit('serverEvent', {
+         type: "played",
+         playerIndex: myPlayerIndex,
+         cellIndex: $(this).index()
+      });
+      //  socket.emit('serverEvent', {type:"sound", playerIndex:myPlayerIndex, cellIndex:$(this).index()});
+   }
 });
 
 socket.on('connected', function (msg) {
    console.log(msg);
-   socket.emit('serverEvent', {type:"reset"});
+   socket.emit('serverEvent', {
+      type: "reset"
+   });
 });
 
 
@@ -97,44 +107,44 @@ socket.on('serverEvent', function (message) {
 
 
    if (message.type == "RandomList") {
-     sounds = message.sounds;
+      sounds = message.sounds;
 
-     let Sound1 = document.getElementById('0')
+      let Sound1 = document.getElementById('0')
 
       Sound1 = sounds[0]
 
-  }
+   }
 
    if (message.type == "reset") {
-       whosTurn = 0;
-       $('.cell').addClass("empty");
-       $('.cell').css("background-color", "white");
+      whosTurn = 0;
+      $('.cell').addClass("empty");
+      $('.cell').css("background-color", "white");
    }
 
    if (message.type == "played") {
+         console.log(sounds[message.cellIndex])
+      if (cardsPlayed == 1) {
+         let cell = $('.wrapper').children()[message.cellIndex];
+         cell = $(cell);
+         cell.removeClass("empty");
+         //cell.css("background-color", playerColors[message.playerIndex]);
+         cell.css("background-color", '#6b6b6b');
+         whosTurn++;
+         cardsPlayed = 0
+         if (whosTurn >= playerCount) {
+            whosTurn = 0;
+         }
+         updateStatus();
 
-   if (cardsPlayed == 1) {
-   let cell = $('.wrapper').children()[message.cellIndex];
-   cell = $(cell);
-   cell.removeClass("empty");
-   //cell.css("background-color", playerColors[message.playerIndex]);
-   cell.css("background-color", '#6b6b6b' );
-   whosTurn++;
-   cardsPlayed = 0
-   if (whosTurn >= playerCount) {
-       whosTurn = 0;
-   }
-   updateStatus();
+      } else {
 
-   } else {
+         let cell = $('.wrapper').children()[message.cellIndex];
+         cell = $(cell);
+         cell.removeClass("empty");
+         cell.css("background-color", '#6b6b6b');
 
-      let cell = $('.wrapper').children()[message.cellIndex];
-      cell = $(cell);
-      cell.removeClass("empty");
-      cell.css("background-color", '#6b6b6b');
-
-   cardsPlayed ++
-}
+         cardsPlayed++
+      }
 
    }
 
@@ -157,12 +167,12 @@ function updateStatus() {
    //$('#player-status').html("There are " + playerCount + " players connected");
 
    $('#playcolor').css("background-color", playerColors[myPlayerIndex]);
-   $('body').css("background-color", playerColors[myPlayerIndex]+"4"); // background color like playing color but less opacity
+   $('body').css("background-color", playerColors[myPlayerIndex] + "4"); // background color like playing color but less opacity
 
    if (whosTurn == myPlayerIndex) {
-       $('.turn-status').html("It's your turn.");
+      $('.turn-status').html("It's your turn.");
    } else {
-       $('.turn-status').html("Waiting for player " + (whosTurn+1) + ".");        
+      $('.turn-status').html("Waiting for player " + (whosTurn + 1) + ".");
    }
 }
 
