8b90a2790e51622a285d374477efbbf7ed141bd5
Phlipper-D <75977142+Phlipper-D@users.noreply.github.com>
2021-03-17 14:44:12 +0100

    14:44

--- client/public/14-Memory/js/script.js
+++ client/public/14-Memory/js/script.js
@@ -19,9 +19,8 @@ let sounds = ['a', 'a', 'b', 'b', 'c', 'c', 'd', 'd', 'e', 'e', 'f', 'f', 'g', '
 //   soundFormats('mp3', 'ogg');
 //   mySound = loadSound('assets/PatchArena_marimba-060.mp3/');
 // }
-function onload(){
-   audio = new Audio('assets/PatchArena_marimba-060.mp3');
-}
+
+audio = new Audio('assets/PatchArena_marimba-060.mp3');
 
 
 
